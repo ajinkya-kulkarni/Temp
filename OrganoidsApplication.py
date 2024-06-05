7204732fca128efb543468bf59f95142ff9c0481
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2024-06-05 14:36:58 +0200

    Update OrganoidsApplication.py

--- OrganoidsApplication.py
+++ OrganoidsApplication.py
@@ -45,7 +45,7 @@ st.title(':blue[Analyze Organoids using OrganoIDNet]')
 
 url1 = "https://doi.org/10.1007/s13402-024-00958-2"
 url2 = "https://github.com/ajinkya-kulkarni/PyOrganoIDNet"
-st.markdown("Refer to :blue[[this publication](%s)] for more information about :blue[[OrganoIDNet](%s)] and its applications." % (url1, url2))
+st.markdown("Refer to :blue[[this publication](%s)] for more information about :blue[[OrganoIDNet](%s)] and its applicability." % (url1, url2))
 
 with st.form(key = 'form1', clear_on_submit = False):
 
